29c3f6a681de75aa68d9f8f94a55de53a5d2fef4
李长健 <lichangjian@itowan.com>
2020-09-22 21:57:38 +0800

    增加树相关算法

--- btree/BNode.go
+++ btree/BNode.go
@@ -12,6 +12,58 @@ type BNode struct {
 	RightChild *BNode
 }
 
+func convert2linkedList(node *BNode) *BNode {
+
+	//newLeftRoot := convert2linkedList(node.LeftChild)
+	//newRightRoot := convert2linkedList(node.LeftChild)
+
+
+}
+
+// 判断两棵树是否相等
+// 相等的条件：树结构一模一样，对应位置的值也相同
+func IsEqual(root1 *BNode, root2 *BNode) bool {
+	if root1 == nil && root2 == nil {
+		return true
+	}
+	if root1 == nil && root2 != nil {
+		return false
+	}
+	if root1 != nil && root2 == nil {
+		return false
+	}
+	if root1.Data == root2.Data {
+		return IsEqual(root1.LeftChild, root2.LeftChild) && IsEqual(root1.RightChild, root2.RightChild)
+	}
+	return false
+}
+
+var maxSum int        // 最大和
+var maxSumNode *BNode // 最大子树的根节点
+
+// 找出和最大的子树
+// 递归求出每一棵子树的和
+func FindMaxSubTree(root *BNode) int {
+	if root == nil {
+		return 0
+	}
+	// 只支持数据类型为整形的树
+	sum, ok := root.Data.(int)
+	if !ok {
+		panic("Type int allowed only")
+	}
+
+	l := FindMaxSubTree(root.LeftChild)
+	r := FindMaxSubTree(root.RightChild)
+	sum += l + r
+	if sum > maxSum {
+		maxSum = sum
+		maxSumNode = root
+	}
+
+	return sum
+}
+
 func LevelPrint(root *BNode, level int) int {
 	if root == nil || level < 0 {
 		return 0
@@ -23,7 +75,7 @@ func LevelPrint(root *BNode, level int) int {
 	}
 }
 
-// 将一个有序数组，转化为有序的树
+// 将一个有序数组，转化为“二元查找树”
 // s 起始下标
 // @param e 结尾下标
 func Arr2Tree(arr []int, s int, e int) *BNode {
--- btree/BNode_test.go
+++ btree/BNode_test.go
@@ -1,6 +1,7 @@
 package btree
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -15,4 +16,39 @@ func TestLevelPrint(t *testing.T) {
 	root := Arr2Tree(arr, 0, len(arr) - 1)
 	h := 3
 	LevelPrint(root, h)
+}
+
+func TestFindMaxSubTree(t *testing.T) {
+	arr := []int{1,-2,-3,4,5,-6,7,-8,9,-10}
+	node := Arr2Tree(arr, 0, len(arr) - 1)
+	FindMaxSubTree(node)
+	fmt.Println("max sum", maxSum)
+	fmt.Println("max sum node", maxSumNode.Data)
+}
+
+func TestIsEqual(t *testing.T) {
+	arr1 := []int{1,-2,-3,4,5,-6,7,-8,9,-10}
+	arr2 := []int{1,-2,-3,4,5,-6,7,-8,9,-10}
+	arr3 := []int{1,-2,-3,4,5,-6,7,-8,9,10}
+
+	root1 := Arr2Tree(arr1, 0, len(arr1) - 1)
+	root2 := Arr2Tree(arr2, 0, len(arr2) - 1)
+	root3 := Arr2Tree(arr3, 0, len(arr3) - 1)
+
+	var bol bool
+
+	bol = IsEqual(root1, root2)
+	if bol == false {
+		t.Error("actually is true")
+	}
+
+	bol = IsEqual(root1, root3)
+	if bol == true {
+		t.Error("actually is false")
+	}
+
+	bol = IsEqual(root2, root3)
+	if bol == true {
+		t.Error("actually is false")
+	}
 }
